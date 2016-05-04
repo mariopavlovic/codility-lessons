fa864f748f76f9add29cdfc0ec3e1d117c4d0440
mpavlovic <mario@undabot.com>
2016-05-04 11:01:43 +0200

    Lesson 10 (B - CountFactors)

--- codility-lessons/codility-lessons/Lesson10.swift
+++ codility-lessons/codility-lessons/Lesson10.swift
@@ -3,7 +3,7 @@ import Foundation
 public class Lesson10 {
     
     public func minPerimeter(N: Int) -> Int {
-        let sqrtN = Int(floor(sqrt(Double(N))))
+        let sqrtN = wholeSqrt(N)
         var min = Int.max
         
         for i in 1 ... sqrtN {
@@ -15,5 +15,32 @@ public class Lesson10 {
         }
         
         return min
+    }
+    
+    public func numFactors(N: Int) -> Int {
+        let sqrtN = wholeSqrt(N)
+        var count = 0
+        
+        for i in 1 ... sqrtN {
+            if N % i == 0 {
+                count += 1
+            }
+        }
+        
+        //check if N is a perfect sqrt (so we don't count twice)
+        if sqrt(Double(N)) == Double(sqrtN) {
+            count = (count * 2) - 1
+        } else {
+            count *= 2
+        }
+        
+        return count
+    }
+}
+
+
+extension Lesson10 {
+    func wholeSqrt(N: Int) -> Int {
+        return Int(floor(sqrt(Double(N))))
     }
 }
--- codility-lessons/tests/Lesson10Specs.swift
+++ codility-lessons/tests/Lesson10Specs.swift
@@ -27,5 +27,28 @@ class Lesson10Specs: QuickSpec {
                 })
             })
         }
+        
+        describe("Given lesson 10 - numFactors") {
+            context("when loaded", {
+                it("should implemenet solution", closure: {
+                    expect(sut.numFactors(4)).toNot(beNil())
+                })
+            })
+            
+            context("when calculating number of factors", { 
+                it("should calculate correctly for small predictable input", closure: { 
+                    expect(sut.numFactors(24)).to(equal(8))
+                    expect(sut.numFactors(16)).to(equal(5))
+                })
+                
+                it("should calculate correctly for small unpredictable input", closure: {
+                    expect(sut.numFactors(1)).to(equal(1))
+                    expect(sut.numFactors(2)).to(equal(2))
+                    expect(sut.numFactors(3)).to(equal(2))
+                    expect(sut.numFactors(9)).to(equal(3))
+                })
+
+            })
+        }
     }
 }
