83f0bc225c82c5e14257e868173b98017e0d9d6f
mpavlovic <mario@undabot.com>
2016-06-20 18:20:42 +0200

    Temp Lesson 16

---
+++ codility-lessons/codility-lessons/Lesson16.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public class Lesson16 {
+    
+}
+
+// MARK: - A
+extension Lesson16 {
+    
+}
---
+++ codility-lessons/tests/Lesson16Specs.swift
@@ -0,0 +1,17 @@
+import Quick
+import Nimble
+
+class Lesson16Specs: QuickSpec {
+    override func spec() {
+        var sut: Lesson16!
+        
+        beforeEach {
+            sut = Lesson16()
+        }
+        
+        describe("Given L16") {
+            describe("when ", {
+            })
+        }
+    }
+}
